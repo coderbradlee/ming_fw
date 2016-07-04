57d7ff0c302ef453f23766963cb027edacd23a34
lzxm160 <lzxm160@sina.com>
2016-07-04 13:20:18 +0800

    test spdlog

--- src/include/ming_log.hpp
+++ src/include/ming_log.hpp
@@ -86,5 +86,5 @@ boost::mutex singleton_ming_log::mu_;
 std::shared_ptr<spd::logger> singleton_ming_log::log_file;
 std::shared_ptr<spd::logger> singleton_ming_log::log_console;
 boost::shared_ptr<singleton_ming_log> ming_log = singleton_ming_log::get_instance();
-typedef singleton_ming_log::get_instance()->log_file log_files;
-typedef singleton_ming_log::get_instance()->log_console log_consoles;
+// typedef singleton_ming_log::get_instance()->log_file log_files;
+// typedef singleton_ming_log::get_instance()->log_console log_consoles;
