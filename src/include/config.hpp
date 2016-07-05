ca47bc9708a9a862609f63f8fa744492273ec9b3
lzxm160 <lzxm160@sina.com>
2016-07-05 13:16:08 +0800

    test design model abstract factory

--- src/include/config.hpp
+++ src/include/config.hpp
@@ -66,8 +66,8 @@ class iconfig:public boost::enable_shared_from_this<iconfig>, boost::noncopyable
 };
 // boost::shared_ptr<iconfig> iconfig::m_ps = nullptr;
 // boost::mutex iconfig::m_mu;
-// boost::shared_ptr<iconfig> get_config = iconfig::get_instance("config.ini");
-#define get_config (iconfig::get_instance("config.ini"));
+ boost::shared_ptr<iconfig> get_config = iconfig::get_instance("config.ini");
+//#define get_config (iconfig::get_instance("config.ini"));
 //boost::shared_ptr<iconfig> get_config = iconfig::get_instance("config.ini");
 #endif
 
