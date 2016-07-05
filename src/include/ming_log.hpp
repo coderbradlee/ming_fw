bd041fbdb9f694443b6a050575c41dab2820f087
lzxm160 <lzxm160@sina.com>
2016-07-05 11:30:55 +0800

    test design model abstract factory

--- src/include/ming_log.hpp
+++ src/include/ming_log.hpp
@@ -111,8 +111,8 @@ boost::mutex singleton_ming_log::mu_;
 // std::shared_ptr<spd::logger> singleton_ming_log::log_file;
 // std::shared_ptr<spd::logger> singleton_ming_log::log_console;
 //#define boost::shared_ptr<singleton_ming_log> ming_log = singleton_ming_log::get_instance();
-//#define ming_log singleton_ming_log::get_instance()
-using ming_log=singleton_ming_log::get_instance();
+#define ming_log singleton_ming_log::get_instance()
+//using ming_log=singleton_ming_log::get_instance();
 // typedef singleton_ming_log::get_instance()->log_file log_files;
 // typedef singleton_ming_log::get_instance()->log_console log_consoles;
 // 
